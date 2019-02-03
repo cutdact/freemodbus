7a6fdf4e517af62199f94a933db635859b110fac
cutdact <mati.jansson@gmail.com>
2019-02-03 21:53:41 +0200

    fixed missing definitions

--- demo/AVR/port/port.h
+++ demo/AVR/port/port.h
@@ -117,22 +117,25 @@ typedef long    LONG;
 #define UPM1            UPM01
 #define UCSRC           UCSR0C
 
-#elif defined (__AVR_ATmega328p__)
-#define UCSRB           UCSR0B
-#define TXEN            TXEN0
-#define RXEN            RXEN0
-#define RXCIE           RXCIE0
-#define TXCIE           TXCIE0
-#define UDRE            UDRE0
-#define UBRR            UBRR0
-#define UCSRC           UCSR0C
-#define UPM1            UPM01
-#define UPM0            UPM00
-#define UCSZ0           UCSZ00
-#define UCSZ1           UCSZ01
-#define UDR             UDR0
-#define SIG_UART_TRANS  SIG_USART_TRANS
-
+#elif defined (__AVR_ATmega328P__)
+#define UCSRB           	UCSR0B
+#define TXEN            	TXEN0
+#define RXEN            	RXEN0
+#define RXCIE           	RXCIE0
+#define TXCIE           	TXCIE0
+#define UDRE            	UDRE0
+#define UBRR            	UBRR0
+#define UCSRC           	UCSR0C
+#define UPM1            	UPM01
+#define UPM0            	UPM00
+#define UCSZ0           	UCSZ00
+#define UCSZ1           	UCSZ01
+#define UDR             	UDR0
+#define SIG_UART_TRANS  	SIG_USART_TRANS
+#define SIG_USART_DATA		USART_UDRE_vect
+#define SIG_USART_RECV		USART_RX_vect
+#define SIGNAL			ISR
+#define SIG_OUTPUT_COMPARE1A	TIMER1_COMPA_vect
 #endif
 
 /* ----------------------- RS485 specifics ----------------------------------*/
--- demo/AVR/port/portserial.c
+++ demo/AVR/port/portserial.c
@@ -33,6 +33,7 @@
 #include "mb.h"
 #include "mbport.h"
 
+
 #define UART_BAUD_RATE          9600
 #define UART_BAUD_CALC(UART_BAUD_RATE,F_OSC) \
     ( ( F_OSC ) / ( ( UART_BAUD_RATE ) * 16UL ) - 1 )
@@ -114,7 +115,7 @@ xMBPortSerialInit( UCHAR ucPORT, ULONG ulBaudRate, UCHAR ucDataBits, eMBParity e
     UCSRC = _BV( URSEL ) | ucUCSRC;
 #elif defined (__AVR_ATmega128__)
     UCSRC |= ucUCSRC;
-#elif defined (__AVR_ATmega328p__)
+#elif defined (__AVR_ATmega328P__)
     UCSRC |= ucUCSRC;
 #endif
 
